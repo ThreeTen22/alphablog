f03a0bd1290078dcef1161ad6bfc8055e0b32f24
ThreeTen22 <gtumen@gmail.com>
2017-06-14 03:02:29 -0500

    set it to path, since things have changed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,11 +5,17 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    #render plain:params[:article].inspect
-
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+
+    if @article.save
+      redirect_to articles_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:format])
   end
 
   private
