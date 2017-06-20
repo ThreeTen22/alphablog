5b0ee50e87596ae6134ef9d3c6ca8389e6aa1d64
ThreeTen22 <gtumen@gmail.com>
2017-06-19 23:38:31 -0500

    created index page, added link_to all over the place

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,22 @@
 class ArticlesController < ActionController::Base
   
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
   end
 
-
   def edit
-    @article = Article.find(params[:format])
+    @article = from_id
   end
 
   def update
-    @article = Article.find(params[:format])
+    @article = from_id
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
-      redirect_to articles_path(@article)
+      redirect_to article_path(@article)
     else
       render 'edit'
     end
@@ -23,21 +26,22 @@ class ArticlesController < ActionController::Base
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to articles_path(@article)
       flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)   
     else
       render 'new'
     end
   end
 
   def show
-    @article = Article.find(params[:format])
+    @article = from_id
   end
 
   private
+    def from_id
+      Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :description)
     end
-  
-
 end
